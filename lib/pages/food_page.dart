ce552a8e17f340423d8487bcbd4aa7d6df10891a
AntoineMHB <antoinemuhumbania@gmail.com>
2024-11-25 13:05:46 +0200

    food page without check box cheked

--- lib/pages/food_page.dart
+++ lib/pages/food_page.dart
@@ -1,3 +1,4 @@
+import 'package:damaz/components/my_button.dart';
 import 'package:damaz/models/food.dart';
 import 'package:flutter/material.dart';
 
@@ -16,10 +17,100 @@ class FoodPage extends StatefulWidget {
 class _FoodPageState extends State<FoodPage> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Image.asset(widget.food.imagePath),
-      ],
-    );
+    return Scaffold(
+      body:  SingleChildScrollView(
+        child: Column(
+          
+        children: [
+        
+          // food image
+          Image.asset(widget.food.imagePath),
+        
+          Padding(
+            padding: const EdgeInsets.all(25.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+              // food name
+            Text(
+              widget.food.name, 
+              style: const TextStyle(
+                fontWeight: FontWeight.bold, 
+                fontSize: 20,
+              ),
+            ),
+        
+            // food price
+            Text(
+              '\$${widget.food.price}',
+              style: TextStyle(
+                fontSize: 16,
+                color: Theme.of(context).colorScheme.primary
+            ),
+            ),
+        
+            const SizedBox(height: 10,),
+            
+            
+            // food description
+            Text(widget.food.description,),
+            
+             const SizedBox(height: 10,),
+        
+             Divider(color: Theme.of(context).colorScheme.secondary,),
+        
+             const SizedBox(height: 10,),
+        
+        
+             Text(
+              "Add-ons", 
+              style: TextStyle(
+                color: Theme.of(context).colorScheme.inversePrimary, 
+                fontSize: 16, 
+                fontWeight: FontWeight.bold),),
+            
+            // addons
+            Container(
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: Theme.of(context).colorScheme.secondary),
+                  borderRadius: BorderRadius.circular(8)),
+              child: ListView.builder(
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                padding: EdgeInsets.zero,
+                itemCount: widget.food.availableAddons.length,
+                itemBuilder: (context, index) {
+                  // get individual addon
+                  Addon addon = widget.food.availableAddons[index];
+              
+                  // return check box UI
+                return CheckboxListTile(
+                  title: Text(addon.name),
+                  subtitle: Text(
+                    '\$${addon.price}', 
+                    style: TextStyle(
+                      color: Theme.of(context).colorScheme.primary),
+                    ),
+                  value: false, 
+                  onChanged: (value) {},
+                );
+              }),
+            )
+            ],),
+          ),
+        
+        
+          // button -> add to the cart
+          MyButton(
+            text: "Add to cart", 
+            onTap: () {},
+          ),
+
+          const SizedBox(height: 25,)
+        ],
+            ),
+      ),
+  );
   }
 }
